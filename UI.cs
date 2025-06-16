e60e0cde26160a058db8cd18c0b6fe1205e7e84a
srullsim <simchonisrael@gmail.com>
2025-06-16 12:23:29 +0300

    edit agent and add UI class

--- UI.cs
+++ UI.cs
@@ -6,9 +6,22 @@ using System.Threading.Tasks;
 
 namespace Sensors
 {
-    internal class UI
+    internal static class UI
     {
 
+
+        // This method displays the menu and returns the user's choice
+        public static string menu()
+        {
+            Console.Clear();
+            Console.WriteLine("======= Welcome to Game Investigation ======= ");
+            Console.WriteLine("please enter the sensor type");
+            Console.WriteLine(" Thermal / Audio / Motion");
+
+            // take choice from player
+            string sensorName = Console.ReadLine();
+            return sensorName;
+        }
 
     }
 }
